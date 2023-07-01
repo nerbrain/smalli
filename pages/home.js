fa4d39439da20d01b89ca93db6674e323de93cb2
nerbrain <malikmox@gmail.com>
2023-07-01 13:03:36 +0300

    first push

---
+++ pages/home.js
@@ -0,0 +1,25 @@
+export default function Home() {
+    return (
+      <>
+        <div className="min-h-full">
+          <header className="bg-white shadow">
+            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+            </div>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+              {/* Your content */}
+              {/* <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+                <label for="URL"  className='text-4xl mx-auto h-10 w-auto '>Smalli</label>
+                <div className="mb-3 pt-0">
+                  <input type="text" placeholder="https://google.com" className="px-3 py-4 placeholder-slate-300 text-slate-600 relative bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" />
+                </div>
+              </div> */}
+  
+            </div>
+          </main>
+        </div>
+      </>
+    )
+  }
