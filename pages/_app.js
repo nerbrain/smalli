0322c312f2353d785ecff6957b981035554b9e5b
nerbrain <malikmox@gmail.com>
2023-07-15 23:50:24 +0300

    more changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Layout from '../components/layout.js'
 import { AuthContext, AuthProvider } from '../stores/AuthContext'
 
 function MyApp({ Component, pageProps }) {
